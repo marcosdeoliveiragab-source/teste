1797cb9ba3ebe446c239bf8c163070e9a0ade037
Marcos Gabriel <marcosdeoliveira.gab@gmail.com>
2026-08-19 16:20:34 -0300

    Add Flask app to download audio from YouTube

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+import yt_dlp
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/baixar')
+def baixar():
+    url = request.args.get('url')
+    opcoes = {'format': 'm4a/bestaudio', 'outtmpl': 'audio.%(ext)s'}
+    with yt_dlp.YoutubeDL(opcoes) as ydl:
+        info = ydl.extract_info(url, download=True)
+        arquivo = ydl.prepare_filename(info)
+    return send_file(arquivo, as_attachment=True)
